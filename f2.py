821fe53cfcb253960a88738c6d2c35728d19dbb2
Dmk90197 <dmk90197@gmail.com>
2024-09-02 20:44:11 +0530

    Second commit

---
+++ f2.py
@@ -0,0 +1,11 @@
+# Define the three numbers
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+number3 = float(input("Enter the third number: "))
+
+# Calculate the average
+average = (number1 + number2 + number3) / 3
+
+# Print the result
+print("The average of the three numbers is:", average)
+
